2ecbf1c8a2fc17d25ac913dfbb7b59f61c35423a
pablo-rr <pablorr2@gmail.com>
2019-05-03 19:04:05 +0200

    objeto interactuable para curarse (falta cambiar el sprite)
    Signed-off-by: pablo-rr <pablorr2@gmail.com>

---
+++ scripts/healthPickup.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+export var healthRestored = 50
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func heal():
+	gameController.health += 50
+	if(gameController.health > 100):
+		gameController.health = 100
+
+func _on_healthPickup_body_entered(body):
+	if(body.name == "player"):
+		heal()
+		get_parent().remove_child(self)
