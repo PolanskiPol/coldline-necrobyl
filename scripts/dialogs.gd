cf9bb45571d34c1f5df07c9581d24bf5c8e1ed8f
pablo-rr <pablorr2@gmail.com>
2019-05-03 19:03:40 +0200

    fix de dialogos + dialogo de pruebas
    Signed-off-by: pablo-rr <pablorr2@gmail.com>

--- scripts/dialogs.gd
+++ scripts/dialogs.gd
@@ -8,9 +8,10 @@ var totalDialogs
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	totalDialogs = get_child_count()
+	print(get_child_count())
 	currentDialog = 1
 	get_node("dialog" + str(currentDialog)).visible = true
-
+	
 # si aun hay cajas de dialogos sin leer, pasa a la siguiente
 func _on_hide():
 	if(currentDialog != totalDialogs):
