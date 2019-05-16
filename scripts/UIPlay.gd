b9046977c985f500c09155405a376dca3677c1fe
pablo-rr <pablorr2@gmail.com>
2019-05-16 09:46:32 +0200

    programada la UI del menu principal
    Signed-off-by: pablo-rr <pablorr2@gmail.com>

---
+++ scripts/UIPlay.gd
@@ -0,0 +1,22 @@
+extends Button
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_play_pressed():
+	var transition = load("res://scenes/effects/transition.tscn").instance()
+	transition.intro = false
+	get_parent().get_parent().get_parent().get_parent().get_parent().add_child(transition)
+	get_parent().get_node("Timer").start()
+	yield(get_parent().get_node("Timer"), "timeout")
+	get_tree().change_scene("res://scenes/levels/intermission" + str(gameController.sceneToGoNumber) + ".tscn")
